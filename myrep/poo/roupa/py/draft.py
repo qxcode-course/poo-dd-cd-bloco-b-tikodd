fcb8f51ddd4ca7c1e33e3b38a4ab41673255359f
Tiago <tiagocm1085@gmail.com>
2025-10-23 14:59:18 -0300

    2h30 fazendo attv roupa

--- myrep/poo/roupa/py/draft.py
+++ myrep/poo/roupa/py/draft.py
@@ -1,21 +1,19 @@
 class Roupa:
     def __init__(self):
         self.__tamanho = ""
-    
+
     def getTamanho(self) -> str:
         return self.__tamanho
 
     def setTamanho(self, valor: str):
         tamanhosValidos = ["PP", "P", "M", "G", "GG", "XG"]
-
         if valor not in tamanhosValidos:
-            print("fail: Valor invalido, tente PP, P, M, G, GG ou XG")
+            print("fail: Valor inválido, tente PP, P, M, G, GG ou XG")
             return
 
         self.__tamanho = valor
 
 roupa = Roupa()
-
 while True:
     try:
         line = input()
@@ -23,16 +21,15 @@ while True:
         break
 
     if not line.strip():
-        # ignora linhas em branco
         continue
 
     args: list[str] = line.split()
-    print(line)
+    print(f"${line}")
 
     cmd = args[0].lower()
     match cmd:
         case "show":
-            print(f"size: {roupa.getTamanho()}")
+            print(f"size: ({roupa.getTamanho()})")
 
         case "size":
             if len(args) < 2:
@@ -44,4 +41,5 @@ while True:
             break
 
         case _:
-            print("fail: comando invalido")
+                print(f"size: {roupa.getTamanho()}")
+            
